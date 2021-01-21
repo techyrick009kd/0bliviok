72912e674f882c6abf7cbe813dc7fa92d185a81c
techyrick009kd <asd5497280@sina.com>
2021-01-21 23:02:30 +0800

    GameUpdate

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1271890.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 60269129, 60269126, 60269127, 60269128, 60269138, 60269684, 60271009, 60271717 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/60269123/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/60269124/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/60269122/1/ammo.js', 'preload' : true},
+];
